2cccfc227618b24667ee5124fe8d36cf7d6785b2
pranaav kumar <pranaavkumarj@gmail.com>
2025-08-04 23:24:01 +0530

    bc to frontend

---
+++ frontend/constants.js
@@ -0,0 +1,5 @@
+import SimpleAbi from "../backend/artifacts/contracts/Simple.sol/Simple.json";
+
+export const CONTRACT_ABI = SimpleAbi.abi; 
+
+export const CONTRACT_ADDRESS = "0x7Dd5DA20bc9a8F6c47eBC01BB5496b75Ab160640";
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,6 @@
+import { ethers } from "ethers";
+import { CONTRACT_ABI, CONTRACT_ADDRESS } from "../constants";
+
 function App() {
   return (
     <div className="min-h-screen bg-blue-600 flex items-center justify-center text-white text-2xl font-bold">
