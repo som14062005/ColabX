d0fd0d11a3e67a9ddf1b275097e3d0ced768a7ce
pranaav kumar <pranaavkumarj@gmail.com>
2025-08-05 10:29:14 +0530

    auto abi export

--- frontend/constants.js
+++ frontend/constants.js
@@ -1,5 +1,5 @@
-import SimpleAbi from "../blockchain/artifacts/contracts/Log.sol/Log.json";
+import LogAbi from "../blockchain/abis/Log.json";
 
-export const CONTRACT_ABI = SimpleAbi.abi; 
+export const CONTRACT_ABI = LogAbi; 
 
 export const CONTRACT_ADDRESS = "0x7Dd5DA20bc9a8F6c47eBC01BB5496b75Ab160640";
